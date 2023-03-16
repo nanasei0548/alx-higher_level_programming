d05ad1d3f38997ad7528c5ed6cbcfef1865b0dfb
nanasei0548 <nanasei0548@gmail.com>
2023-03-16 07:33:44 +0000

    sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+--creates a table force_name table in the current database,
+-- do nothing if the force_name table already exists.
+
+CREATE TABLE IF NOT EXISTS `force_name` (
+	`id` INT,
+	`name` VARCHAR(256) NOT NULL
+);
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- creates a table unique_id table in the current database,
+-- do nothing if the unique_id table already exists.
+
+CREATE TABLE IF NOT EXISTS `unique_id` (
+	`id` INT NOT NULL DEFAULT 1 UNIQUE,
+	`name` VARCHAR(256)
+);
