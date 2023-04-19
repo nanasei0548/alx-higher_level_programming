318c9bc2372d2a7935f6dff69443614ba2e1c783
nanasei0548 <nanasei0548@gmail.com>
2023-04-19 09:21:01 +0000

    fixed

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-"""
-Script that lists all `City` objects from the database `hbtn_0e_101_usa`.
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
-"""
 
-import sys
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
-from sqlalchemy.engine.url import URL
 from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
 
 
-if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-
-    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
-           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
-
-    engine = create_engine(URL(**url), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(bind=engine)
-
-    cities = session.query(City)
-
-    for city in cities:
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, data))
+    # create custom session object class from database engine
+    Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
+    session = Session()
+    rows = session.query(City).all()
+    for city in rows:
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    session.close()
