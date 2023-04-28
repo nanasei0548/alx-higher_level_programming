f92d54c99ee4bcdbfc5f91c75e94d9d1fd256b9f
nanasei0548 <nanasei0548@gmail.com>
2023-04-28 16:37:32 +0000

    network

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,7 +8,7 @@ from requests import get
 
 
 if __name__ == "__main__":
-    url = 'https:/alx-intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     response = get(url)
     bytes_content = response.text
     string = 'Body response:\n\t- type: {}\n\t- content: {}'.format(
